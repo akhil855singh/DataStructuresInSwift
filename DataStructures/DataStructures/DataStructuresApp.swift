169b81229782058b9d27c6de1cfa3ffdade3db9f
Akhil <akhil.singh@zeniuseducation.com>
2023-11-14 13:32:56 +0530

    added files

---
+++ DataStructures/DataStructures/DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresApp.swift
+//  DataStructures
+//
+//  Created by Akhil on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
